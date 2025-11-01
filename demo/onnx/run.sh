90bcc1a49d9b5df7fa198ff87bf176ef993825c1
王炎生 <yswang@listenai.com>
2025-11-01 16:02:49 +0800

    [add] add vscode launch.json

--- demo/onnx/run.sh
+++ demo/onnx/run.sh
@@ -10,3 +10,24 @@ iree-compile     --iree-hal-target-device=local     --iree-hal-local-target-devi
 #iree-compile --iree-llvmcpu-list-targets
 iree-run-module     --device=local-task     --module=mobilenet_cpu.vmfb     --function=torch-jit-export     --input="1x3x224x224xf32=0"
 iree-run-module     --device=local-task     --module=mobilenet_cpu.vmfb     --function=torch-jit-export     --input="2x3x224x224xf32=0"
+
+mkdir -p /tmp/iree/mobilenetv2/
+iree-compile mobilenetv2.mlir \
+  --iree-hal-target-device=local \
+  --iree-hal-local-target-device-backends=llvm-cpu \
+  --iree-llvmcpu-target-cpu=generic   \
+  --iree-llvmcpu-link-embedded=false \
+  --dump-compilation-phases-to=/tmp/iree/mobilenetv2 \
+  -o /tmp/iree/simple_abs/mobilenetv2.vmfb
+
+# ./../../iree-build/tools/iree-compile mobilenetv2.mlir \
+#   --iree-hal-target-device=local   \
+#   --iree-hal-local-target-device-backends=llvm-cpu   \
+#   --iree-llvmcpu-target-cpu=generic     \
+#   --iree-llvmcpu-link-embedded=false  \
+#   --compile-to=input    \
+#   --mlir-print-ir-after-all \
+#   --mlir-print-ir-after-change  \
+#   --mlir-print-ir-before-all  > 1.txt 2>&1
+
+# grep Before 1.txt 
